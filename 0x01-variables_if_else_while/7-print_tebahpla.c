296c1abc7c7a87711a36f30e9c797f05eebc09db
PeaceTobi <peaceshalom9@gmail.com>
2023-04-23 09:24:46 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -prints the alphabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch++)
+		putchar(ch);
+
+	purchar('\n');
+
+	return (0);
+}
+
