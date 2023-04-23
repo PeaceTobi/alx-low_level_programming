e4160d65a6028e5c18f849a5ed7d9718f30e7bdb
PeaceTobi <peaceshalom9@gmail.com>
2023-04-23 08:58:38 -0700

    Pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -prints the alphabet in lowercasw
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
